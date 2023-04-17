59d4a0b871441da124ad9914a61dede8c8dfe90f
yogesh0802 <yogeshmborole3309@gmail.com>
2023-04-17 18:10:48 +0530

    9th

--- model/doctorModel.js
+++ model/doctorModel.js
@@ -21,9 +21,9 @@ const doctorSchema = new mongoose.Schema(
       type: String,
       required: [true, "Email is required"],
     },
-    website: {
-      type: String,
-    },
+    // website: {
+    //   type: String,
+    // },
     address: {
       type: String,
       required: [true, "Adrress is required"],
